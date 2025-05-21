31a463504e8747d3f2b1500aa4b84fc19b37328f
udashsanam <udashsanam@gmail.com>
2025-05-21 15:29:27 +0545

    add: initi project

---
+++ src/main/java/com/learn/liqubase/LiqubaseApplication.java
@@ -0,0 +1,13 @@
+package com.learn.liqubase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiqubaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiqubaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/liqubase/LiqubaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.liqubase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiqubaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
